c72768abaec3ae6913103b6fd0a802ebf5565f6b
Evashasha <673222391@qq.com>
2020-03-01 23:45:30 +0800

    Site updated: 2020-03-01 23:45:22

--- photos/ins.js
+++ photos/ins.js
@@ -125,7 +125,7 @@
   
             liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                    <img class="reward-img" data-type="' + type + '" data-src="' + src + '" src="./assets/empty.jpeg" itemprop="thumbnail" onload="lzld(this)">\
+                    <img class="reward-img" data-type="' + type + '" data-src="' + src + '" src="./assets/empty.jpg" itemprop="thumbnail" onload="lzld(this)">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
               </figure>';
